0567319adb6417f4e773266a6cb87263b09a67ac
harrynewt0n <harry-newton@outlook.com>
2024-01-16 00:10:53 +0000

    Added questions 3, 4 and 5

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -12,21 +12,21 @@ var quizQuestions = [
 
     }
     {
-        prompt: 'In JavaScript, what element is used to store multiple values in a single variable?',
-        answers: ['Arrays', 'Strings', 'Functions', 'Variables'],
-        correct: 'Arrays'
+        prompt: 'What is the type of loop that continues through a block of code as long as the specified condition remains TRUE? A (BLANK) Loop',
+        answers: ['While', 'Conditional', 'For', 'Else'],
+        correct: 'Conditional'
 
     }
     {
-        prompt: 'In JavaScript, what element is used to store multiple values in a single variable?',
-        answers: ['Arrays', 'Strings', 'Functions', 'Variables'],
-        correct: 'Arrays'
+        prompt: 'What is the format called that is used for storing and transporting data?',
+        answers: ['Syntax', 'Font', 'HTML', 'JSON'],
+        correct: 'JSON'
 
     }
     {
-        prompt: 'In JavaScript, what element is used to store multiple values in a single variable?',
-        answers: ['Arrays', 'Strings', 'Functions', 'Variables'],
-        correct: 'Arrays'
+        prompt: 'Which of these is the correct format convention for a variable?',
+        answers: ['yourvariable', 'YOURVARIABLE', 'yourVariable', 'YourVariable'],
+        correct: 'yourVariable'
 
     }
 ]
