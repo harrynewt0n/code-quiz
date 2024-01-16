a617e64ef57c4d659aa62e9ade8da129e7a796d0
harrynewt0n <harry-newton@outlook.com>
2024-01-16 00:02:02 +0000

    Added questions 1 and 2 to questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+var quizQuestions = [
+    {
+        prompt: 'In JavaScript, what element is used to store multiple values in a single variable?',
+        answers: ['Arrays', 'Strings', 'Functions', 'Variables'],
+        correct: 'Arrays'
+
+    }
+    {
+        prompt: 'What is considered to be the most popular programming language in the world?',
+        answers: ['JavaScript', 'Python', 'HTML', 'CSS'],
+        correct: 'JavaScript'
+
+    }
+    {
+        prompt: 'In JavaScript, what element is used to store multiple values in a single variable?',
+        answers: ['Arrays', 'Strings', 'Functions', 'Variables'],
+        correct: 'Arrays'
+
+    }
+    {
+        prompt: 'In JavaScript, what element is used to store multiple values in a single variable?',
+        answers: ['Arrays', 'Strings', 'Functions', 'Variables'],
+        correct: 'Arrays'
+
+    }
+    {
+        prompt: 'In JavaScript, what element is used to store multiple values in a single variable?',
+        answers: ['Arrays', 'Strings', 'Functions', 'Variables'],
+        correct: 'Arrays'
+
+    }
+]
